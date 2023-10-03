82d9d13f88cdcb0b8fb6d65e8adae3c2cd236cb7
sakhawat71 <sakhawat.showrav@gmail.com>
2023-10-03 22:47:47 +0600

    widhdraw part complete

---
+++ js/withdraw.js
@@ -0,0 +1,38 @@
+const withdraw = ()=>{
+    
+    // get input value
+    const withdrawInput = document.getElementById("input-withdraw");
+    const withdrawInputString = withdrawInput.value;
+    const newWithdrawValue = parseFloat(withdrawInputString);
+
+    // get inner text form 
+    const withdrawMoney = document.getElementById("withdraw-box");
+    const withdrawMoneyString = withdrawMoney.innerText;
+    const previousWithdrawMoney = parseFloat(withdrawMoneyString);
+    
+    withdrawInput.value = "";
+    if(isNaN(newWithdrawValue)){
+        alert("try to use Number")
+        return;
+    }
+
+    // withdraw box calculet
+    const withdrawTotal = previousWithdrawMoney + newWithdrawValue;
+    withdrawMoney.innerText = withdrawTotal;
+
+    // grand total form id
+    const grandTotal = document.getElementById("total-balance");
+    const grandTotalString = grandTotal.innerText;
+    const previouseGrandTotal = parseFloat(grandTotalString);
+    
+
+    if(previouseGrandTotal < previouseGrandTotal){
+        alert("not enough money")
+        return;
+    }
+    //
+    const newGrandTotal = previouseGrandTotal - newWithdrawValue;
+    grandTotal.innerText = newGrandTotal;
+
+    withdrawInput.value = "";
+}
